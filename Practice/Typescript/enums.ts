c7682a9d391e86319c4b6b950fe3ea5524a39b97
Rushi <rushikeshshelar.cs@gmail.com>
2024-02-02 23:59:28 +0530

    Typescript Practice

---
+++ Practice/Typescript/enums.ts
@@ -0,0 +1,17 @@
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
+function doSomething(key: Direction) {
+	// do something.
+
+}
+
+doSomething(Direction.Up);
+doSomething(Direction.Down);
+console.log(Direction.Up); // 0
+console.log(Direction.Down);
+
+
---
+++ Practice/Typescript/generics.ts
@@ -0,0 +1,1 @@
+// Generics in Typescript
